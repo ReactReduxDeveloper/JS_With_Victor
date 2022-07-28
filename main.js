ced1ac059f4d4164c9155dbb51fbc2796ad28e2e
ReactReduxDeveloper <cool.pomolov@ya.ru>
2022-07-28 21:16:41 +0300

    5_lesson

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
-const id_1 = "45gh-56yu"
-const id_2 = "55gh-58yu"
-const todolists = [
-
+const names = ["Bob", "Alex", "Dima"]
+const numbers = [1, 2, 39, 34, 357]
+console.log(names.sort())
+const compareFn = (a, b) => a + b
+console.log(numbers.sort(compareFn).reverse())
+const students = [
     {
-        id: id_1,
-        title: "What to learn",
-        filter: "all",
-        tasks: [
-            {id: 12, title: "JS", isDone: false},
-            {id: 13, title: "TS", isDone: false},
-            {id: 14, title: "HTML", isDone: true},
-        ]
+        name: "Bob",
+        age: 20,
+        isMarride: true,
+        scores: 89
     },
     {
-        id: id_2,
-        title: "What to buy",
-        filter: "all",
-        tasks: [
-            {id: 22, title: "BEER", isDone: false},
-            {id: 23, title: "Meat", isDone: false},
-            {id: 24, title: "Fish", isDone: false},
-        ]
+        name: "Alex",
+        age: 25,
+        isMarride: false,
+        scores: 300
+    },
+    {
+        name: "Gera",
+        age: 54,
+        isMarride: true,
+        scores: 34
+    },
+    {
+        name: "Drug",
+        age: 50,
+        isMarride: true,
+        scores: 65
+    },
+];
+console.log(students.sort((a, b) => a.scores - b.scores).reverse())
+console.log(students.sort((a, b) => a.name.localeCompare(b.name)))
+console.log(students.sort((a, b) => a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1))
+
+const nums = [54, 11, 36, 67, 89, 27]
+
+for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]){
+        const temp = nums[i]
+        nums[i] = nums[i + 1]
+        nums[i + 1] = temp
     }
-]
-const tasks = {
-    [id_1]: [
-        {id: 12, title: "JS", isDone: false},
-        {id: 13, title: "TS", isDone: false},
-        {id: 14, title: "HTML", isDone: true},
-    ],
-    [id_2]: [
-        {id: 22, title: "BEER", isDone: false},
-        {id: 23, title: "Meat", isDone: false},
-        {id: 24, title: "Fish", isDone: false},
-    ]
-}
-console.log(tasks[id_1].find(t => t.id === 12).title)
-console.log(...tasks[id_2], {id: 25, title: "MILK", isDone: false})
-console.log(tasks[id_1].map(t => t.title))
-const nums = [50, 40, 30]
-console.log(nums.reduce((arr, el) => arr * el, 2))
-console.log(nums.reduce((arr, el) => arr > el ? el : arr))
+        }
